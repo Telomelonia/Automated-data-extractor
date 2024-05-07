8efcbc5472958235c1c0a50cd848fe63c9b978f2
telomelonia <shuklas2002@gmail.com>
2024-05-07 14:59:03 +0530

    add pdf feature and data process

--- webdriver/driver.py
+++ webdriver/driver.py
@@ -2,25 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
+import PyPDF2
 
 service = Service(executable_path='webdriver\chromedriver-win64\chromedriver.exe')
 options = webdriver.ChromeOptions()
 
 driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://www.sciencedirect.com/science/article/pii/S1359646222005486")
-time.sleep(5)
-# Get the HTML source of the page
-html_source = driver.page_source
+url = "https://www.sciencedirect.com/science/article/pii/S0924013698004506"
 
-# Parse the HTML source using BeautifulSoup
-soup = BeautifulSoup(html_source, 'html.parser')
+if url.endswith('.pdf'):
+    pdf_reader = PyPDF2.PdfFileReader(driver.current_url)
+    
+    # Extract text from each page
+    for page in pdf_reader.pages:
+        print(page.extractText())
+else:        
+    driver.get(url)
+    time.sleep(5)
+    
+    # Get the HTML source of the page
+    html_source = driver.page_source
 
-# Example: Print the title of the page
-print("Title:", soup.title)
-driver.quit()
+    # Parse the HTML source using BeautifulSoup
+    soup = BeautifulSoup(html_source, 'html.parser')
+
+    # Example: Print the title of the page
+    print("Title:", soup.text)
+    driver.quit()
