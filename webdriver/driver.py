bb47f92a7abcc2ac377b9aefd0a60625da6633d0
telomelonia <shuklas2002@gmail.com>
2024-05-07 14:14:38 +0530

    add html parser

--- webdriver/driver.py
+++ webdriver/driver.py
@@ -6,11 +6,21 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
 
 service = Service(executable_path='webdriver\chromedriver-win64\chromedriver.exe')
 options = webdriver.ChromeOptions()
-def url_opener(url):
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get(url)
-    time.sleep(5)
-    driver.quit()
+
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("https://www.sciencedirect.com/science/article/pii/S1359646222005486")
+time.sleep(5)
+# Get the HTML source of the page
+html_source = driver.page_source
+
+# Parse the HTML source using BeautifulSoup
+soup = BeautifulSoup(html_source, 'html.parser')
+
+# Example: Print the title of the page
+print("Title:", soup.title)
+driver.quit()
